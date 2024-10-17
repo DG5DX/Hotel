1773359803d9b2b30c4718139a776e0c3e0c9ca4
DG5DX <dafecano@gmail.com>
2024-10-17 07:12:28 -0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="Contenedor">
     <div class="Tab">
-    <q-tabs v-model="tab" no-caps class="bg-orange text-white shadow-2">
-      <q-tab name="Inicio" label="Inicio" @click="goTo('/')"/>
-      <q-tab name="Habitaciones" label="Habitaciones" @click="goTo('/habitacion')"/>
-      <q-tab name="Servicios" label="Servicios" @click="goTo('/servicio')" />
-      <q-tab name="Actividades" label="Actividades" @click="goTo('/actividad')"/>
-      <q-tab name="Registrarse" label="Registrarse" @click="goTo('/login')"/>
-    </q-tabs>
+      <q-tabs v-model="tab" no-caps class="bg-orange text-white shadow-2">
+        <q-tab name="Inicio" label="Inicio" @click="goTo('/')" />
+        <q-tab name="Habitaciones" label="Habitaciones" @click="goTo('/habitacion')" />
+        <q-tab name="Servicios" label="Servicios" @click="goTo('/servicio')" />
+        <q-tab name="Actividades" label="Actividades" @click="goTo('/actividad')" />
+        <q-tab name="Registrarse" label="Registrarse" @click="goTo('/login')" />
+      </q-tabs>
     </div>
     <router-view></router-view>
+
+    <footer class="Pie">
+      <div class="Info_Contacto">
+        <p>Teléfono: +57 304 3526113</p>
+        <p>Email: dafecani@gmail.com</p>
+      </div>
+      <div class="Redes">
+        <p>Redes Sociales:</p>
+        <p>
+          <a href="https://example.com/" target="_blank">Facebook</a> |
+          <a href="https://example.com/" target="_blank">Twitter</a> |
+          <a href="https://example.com/" target="_blank">Instagram</a>
+        </p>
+      </div>
+      <p class="Copy">&copy; 2024 Hotel valle del sol.</p>
+    </footer>
   </div>
 </template>
+
 <script setup>
 import { ref } from 'vue';
-import { useQuasar } from 'quasar';
-import axios from 'axios';
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 
 const tab = ref('Inicio');
 const router = useRouter();
@@ -24,13 +39,45 @@ const goTo = (route) => {
   router.push(route);
 }
 </script>
+
 <style>
 .Contenedor {
   background-image: url(https://st2.depositphotos.com/2974663/46714/i/450/depositphotos_467149506-stock-photo-wood-texture-background-vector-illustration.jpg);
 }
-.Tab{
+
+.Tab {
   position: sticky;
   top: 0;
   z-index: 1000;
+}
+
+.Pie {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 7px;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+}
+
+.Info_Contacto,
+.Redes {
+  display: grid;
+  justify-content: center;
+}
+.Copy{
+  display: grid;
+  align-items: center;
+}
+a{
+  color: aqua;
+}
+
+@media (max-width:835px) {
+  .Pie {
+    display: grid;
+  }
 }
 </style>
